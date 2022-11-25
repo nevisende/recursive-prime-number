04fa49b760ac93bb63095b9b8e0f48eab1f5cf41
furkandenizhan <ekonomitakipmail@gmail.com>
2022-11-25 20:56:56 +0300

    Feat: add Main class

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        InteractToUser interactToUser = new InteractToUser();
+        interactToUser.mainFuncLoop();
+    }
+
+}
